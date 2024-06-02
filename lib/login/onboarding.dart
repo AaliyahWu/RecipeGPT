c97e321eeb803a4593610d0d28bc2dd8e247aa30
Yun087 <annie2003080782@gmail.com>
2024-06-03 03:50:05 +0800

    前置頁面完成!

--- lib/login/onboarding.dart
+++ lib/login/onboarding.dart
@@ -41,11 +41,11 @@ class _OnboardingSlideshowState extends State<OnboardingSlideshow> {
               children: <Widget>[
                 buildPage(
                     '\n你是否曾面對著食材滿當當的冰箱，卻不知道該煮什麼?\n一直一直買新的食材，卻造成許多食物過期而浪費。\n那就快來使用好食在吧！\n',
-                    'assets/LOGO.png',
+                    'assets/Page1.jpg',
                     '跳過'),
-                buildPage('\n透過影像辨識及可自動生成食譜的功能，\n輕易解決糧食浪費問題，\n讓你每天都有新菜色，不僅吃的開心還健康。\n',
-                    'assets/LOGO.png', '跳過'),
-                buildPage('Page 3', 'assets/LOGO.png', '登入'),
+                buildPage('\n透過影像辨識及可自動生成食譜的功能\n輕易解決糧食浪費問題\n讓你每天都有新菜色，不僅吃的開心還健康。\n',
+                    'assets/Page2.jpg', '跳過'),
+                buildPage('\n感興趣嗎！快點擊下方按鈕\n立即加入「好食在」這個大家庭吧！', 'assets/Page3.jpg', '登入'),
               ],
             ),
           ),
@@ -100,8 +100,8 @@ class _OnboardingSlideshowState extends State<OnboardingSlideshow> {
         children: <Widget>[
           Image.asset(
             imagePath,
-            width: 150,
-            height: 150,
+            width: 200,
+            height: 200,
           ), // 添加圖片
           Text(
             text,
