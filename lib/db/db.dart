e073f9969f9594c91c04800537a627e864ea4d6c
shuang <626aaliyah626@gmail.com>
2024-08-21 21:54:33 +0800

    刪除-刪除舊有資料庫連線資訊

--- lib/db/db.dart
+++ lib/db/db.dart
@@ -30,32 +30,3 @@ class DatabaseService {
   }
 }
 
-// class DatabaseService {
-//   static final DatabaseService _instance = DatabaseService._internal();
-//   MySqlConnection? _connection;
-
-//   factory DatabaseService() {
-//     return _instance;
-//   }
-
-//   DatabaseService._internal();
-
-//   Future<MySqlConnection> get connection async {
-//     if (_connection == null) {
-//       final settings = ConnectionSettings(
-//         host: 'recipeedb-2.mysql.database.azure.com',
-//         port: 3306,
-//         user: 'yuntech',
-//         password: 'recipeDB@@',
-//         db: 'recipedb',
-//       );
-//       _connection = await MySqlConnection.connect(settings);
-//     }
-//     return _connection!;
-//   }
-
-//   Future<void> closeConnection() async {
-//     await _connection?.close();
-//     _connection = null;
-//   }
-// }
